b208bcc22c7153df552d966f21c9765b9ed19485
Mario Cesena <112787277+mariocc22@users.noreply.github.com>
2023-10-09 09:35:15 -0700

    feat: Adding the Document Picker and FileSystem Reader

--- src/screens/BottomTabScreens/Study/index.jsx
+++ src/screens/BottomTabScreens/Study/index.jsx
@@ -3,12 +3,50 @@ import { View, Text, StyleSheet, Pressable } from "react-native";
 // react navigation imports
 import { useNavigation } from "@react-navigation/native";
 
+// react hooks
+import { useState } from "react";
+
+// pdf reader
+import * as DocumentPicker from "expo-document-picker";
+import * as FileSystem from "expo-file-system";
+
 const Study = () => {
   const { navigate } = useNavigation();
+  const [pdfContent, setPdfContent] = useState("");
+
+  const handleUploadPDF = async () => {
+    try {
+      const res = await DocumentPicker.getDocumentAsync();
+      console.log(res);
+      const pdfUri = res.assets[0].uri;
+      const pdfBase64 = await FileSystem.readAsStringAsync(pdfUri, {
+        encoding: "base64",
+      });
+      console.log("this is the pdfBas64", pdfBase64);
+      // setPdfContent(pdfBase64);
+    } catch (error) {
+      console.log(error?.message);
+    }
+  };
 
   return (
     <View style={styles.container}>
       <Text>Study</Text>
+      <Pressable
+        onPress={handleUploadPDF}
+        style={{
+          padding: 20,
+          backgroundColor: "#000",
+          borderRadius: 5,
+          margin: 50,
+          width: 200,
+        }}
+      >
+        <Text style={{ color: "white", textAlign: "center" }}>
+          Upload a file PDF
+        </Text>
+      </Pressable>
+      <Text>{pdfContent}</Text>
       <Pressable
         style={{ marginBottom: 20 }}
         onPress={() => navigate("KeyTopic")}
