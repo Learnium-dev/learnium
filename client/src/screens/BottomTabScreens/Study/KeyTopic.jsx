07af36f5533b9256d1e72cad22e419addaa7f200
eugenia-chornobai <119853623+Eugenia-Chornobai@users.noreply.github.com>
2023-10-12 09:15:23 -0700

    PT-32 Flash Card Create (#3)
    * Create Flash Cards from Json object
    
    * Revert "Create Flash Cards from Json object"
    
    This reverts commit 2b4438a0f9f4750e15d075a810673d25e854fee2.
    
    * PT-32 Flash Card Create
    
    * PT-32 Remove yarn

--- client/src/screens/BottomTabScreens/Study/KeyTopic.jsx
+++ client/src/screens/BottomTabScreens/Study/KeyTopic.jsx
@@ -1,10 +1,65 @@
-import { View, Text, StyleSheet } from "react-native";
+import { View, Text, StyleSheet, Button } from "react-native";
+import { useState, useRef, useMemo, useCallback } from 'react';
+import { BottomSheetModal } from '@gorhom/bottom-sheet';
+import FlashCardsContainer from '../../../containers/FlashCardsContainer';
 
-const KeyTopic = () => {
+const dummyCards = [
+  {
+    answer: "E-commerce, cloud computing, and digital streaming",
+    question: "What does Amazon specialize in?",
+    difficult: false,
+  },
+  {
+    answer: "Jeff Bezos in 1994",
+    question: "Who founded Amazon and when?",
+    difficult: false,
+  },
+  {
+    answer: "Bellevue, Washington",
+    question: "Where was Amazon founded?",
+    difficult: false,
+  },
+  {
+    answer: "Zoox, Amazon Lab126, and Kuiper Systems among others",
+    question: "What are some of Amazon’s subsidiaries?",
+    difficult: false,
+  },
+  {
+    answer: "Books",
+    question: "What was Amazon initially an online marketplace for?",
+    difficult: false,
+  },
+];
+
+const KeyTopic = (props) => {
+
+  const bottomSheetModalRef = useRef(null);
+
+  // height of the bottom sheet modal 95%
+  const snapPoints = useMemo(() => ['95%'], []);
+
+  const openBottomSheet = useCallback(() => {
+    bottomSheetModalRef.current?.present();
+  }, []);
+
+  const closeBottomSheet = () => {
+    bottomSheetModalRef.current.dismiss();
+  }
+
   return (
-    <View style={styles.container}>
-      <Text>KeyTopic</Text>
-    </View>
+
+      <View style={styles.container}>
+        <Text>KeyTopic</Text>
+        <Button title={'View Topic Flashcards'} onPress={openBottomSheet} />
+        <BottomSheetModal
+          ref={bottomSheetModalRef}
+          index={0}
+          snapPoints={snapPoints}
+        >
+          <FlashCardsContainer flashcards={dummyCards} closeSheet={closeBottomSheet} />
+        </BottomSheetModal>
+      </View>
+
   );
 };
 
