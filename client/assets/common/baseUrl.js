7e4146a31cfcd996f4a03d188aa9d7f726b4b7d3
Edgar Velandia <alvelx@hotmail.com>
2023-10-18 11:01:07 -0700

    PT-185 API Test page - consume API with token from Frontend

--- client/assets/common/baseUrl.js
+++ client/assets/common/baseUrl.js
@@ -4,7 +4,7 @@ import { Platform } from 'react-native'
 let baseURL = '';
 
 {Platform.OS == 'android'
-? baseURL = 'http://192.168.1.77:3000/api/v1/'
+? baseURL = 'http://192.168.30.16:3000/api/v1/'
 : baseURL = 'http://10.0.2.2:3000/api/v1/'
 }
 
---
+++ client/src/screens/User/TestAPI.jsx
@@ -0,0 +1,44 @@
+import React, { useEffect, useState } from "react"
+import { 
+    View, 
+    Text,
+} from "react-native"
+import baseURL from "../../../assets/common/baseUrl";
+import axios from "axios";
+import AsyncStorage from "@react-native-async-storage/async-storage";
+
+const TestAPI = (props) => {
+
+    const [quizzes, setQuizzes] = useState([]);
+    const [token, setToken] = useState();
+
+    // Get token
+    AsyncStorage.getItem('jwt').then((token) => {
+        if(token){
+            console.log(token);
+            setToken(token);
+        }
+    });
+
+    // Consume API with token
+    axios
+        .get(`${baseURL}quizzes`, {
+            headers: { Authorization: `Bearer ${token}` },
+        })
+        .then((res) => {
+            console.log(res.data);
+            setQuizzes(res.data);   
+            }
+        )
+        .catch((error) => {
+            console.log(error);
+        });
+
+    return (
+        <View>
+            <Text>{token}</Text>
+        </View>
+    )
+}
+
+export default TestAPI;
