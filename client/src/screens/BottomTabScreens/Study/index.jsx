b0e797d560a05e062b3fd9c4d5b6b9433627035b
Tony-Thawatchai Saengthamchai <au.thawatchai@gmail.com>
2023-10-18 20:58:23 -0700

    PT-177:fix:data model for AI response (#14)
    Co-authored-by: Mario Cesena <112787277+mariocc22@users.noreply.github.com>

--- client/src/screens/BottomTabScreens/Study/index.jsx
+++ client/src/screens/BottomTabScreens/Study/index.jsx
@@ -29,6 +29,7 @@ const Study = () => {
       });
 
       const response = await axios.post(
+
         "http://192.168.1.155:3000/upload-pdf",
         formData,
         {
--- server/controllers/uploadcontent.js
+++ server/controllers/uploadcontent.js
@@ -9,40 +9,40 @@ const uploadContent = async (req, res) => {
     const openai = new OpenAI({
       apiKey: AIKEY,
     });
-    
+
     const completion = await openai.chat.completions.create({
       model: "gpt-4",
       messages: [
         { role: "system", content: "You are a education instructor." },
         {
           role: "user",
-          content: `please summarize this content from ${req.body.article} 
+          content: `please analyze this content from ${req.body.article} 
           and return me in JSON format with the following information:
-          1. summary of the article at roughly 20 words.
-              2. key topic that important for me to know before taking the exam. Key topic can be as many as necessary.
-          3. Create a question and answer for me to test my knowledge at 5 questions, each question should have 4 choices and related to each key topic. in JSON format with key {question: "question", answer: "answer", choices: ["choice1", "choice2", "choice3", "choice4"], keyTopic: "keyTopic"}
-          4. Create a flashcard for me to study at 5-10 flashcards depend on how large the article is. Return the flashcard in a format of question and answer.
+          1. key topic that important for taking the exam. amount can be as many as necessary.
+          2. summary of each key topic.
+          3. For each key topic, create 5 questions to test my knowledge, each question should have 4 choices. 
+          4. For each key topic, Create a flashcard for me to study at 5-10 flashcards. 
           Plese format JSON object as follow:
-          {
-            summary: "summary",
-            keyTopic: ["keyTopic1", "keyTopic2",....],
-            questionAnswer: [
-              {question: "question", answer: "answer", choices: ["choice1", "choice2", "choice3", "choice4"], keyTopic: "keyTopic"},
-            ],
-            flashcard: [
-              {question: "question", answer: "answer"},
-            ]
-          }
-          `
+          [
+             {
+              keyTopic: "title",
+              summary: "summary",
+              questionAnswer: [
+                {question: "question", answer: "answer", choices: ["choice1", "choice2", "choice3", "choice4"]
+              ],
+              flashcard: [
+                {question: "question", answer: "answer"},
+              ]
+            }
+          ]
+          `,
         },
       ],
     });
     console.log(completion.choices[0]);
-    
+
     const result = completion.choices[0].message.content;
     res.json(result);
-    // res.json({a:"test xxxxxx", b:"test2"})
-     
   } catch (error) {
     console.log(error);
     res.status(500).json({ error: "Database Error" });
