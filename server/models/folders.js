90eed1ed0bed8692711fdd9b0b9ee1c147de0ab1
Edgar Velandia <evelandiarodri00@mylangara.ca>
2023-10-21 19:43:23 -0700

    PT-195 openAI POST response > alex optimize API to make it faster

--- server/models/folders.js
+++ server/models/folders.js
@@ -4,13 +4,31 @@ const mongoose = require('mongoose');
 const folderSchema = mongoose.Schema({
   name: {
     type: String,
-    lowercase: true,
+    lowercase: false,
     required: [true, "Folder name is required"],
   },
   userid: {
     type: mongoose.ObjectId,
     ref: "users",
     required: [true, "User Id is required"],
+  },
+  content: {
+    type: String,
+    lowercase: false,
+    required: [true, "Content is required"],
+  },
+  url: {
+    type: String,
+    lowercase: true,
+    required: [true, "Url is required"],
+  },
+  exam: {
+    type: Boolean,
+    default: false,
+  },
+  examdate: {
+    type: Date,
+    default: Date.now,
   },
 });
 
