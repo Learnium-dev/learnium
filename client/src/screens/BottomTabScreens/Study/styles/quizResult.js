e9d84eb4331ac6814dbf0997c2cbd34840ab4f82
Mario Cesena <112787277+mariocc22@users.noreply.github.com>
2023-11-02 23:12:36 -0700

    feat: Merge after dev

---
+++ client/src/screens/BottomTabScreens/Study/styles/quizResult.js
@@ -0,0 +1,11 @@
+import { StyleSheet } from "react-native";
+
+export const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    paddingTop: 20,
+    paddingBottom: 20,
+    paddingHorizontal: 16,
+    backgroundColor: "#fff",
+  },
+});
