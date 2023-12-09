e5c712584d8979991904de75c802fe2aad559969
Mario <mceseahurtado00@mylangara.ca>
2023-12-08 21:12:47 -0800

    chore: Adding the new API endpoint from our AWS instance

--- client/assets/common/baseUrl.js
+++ client/assets/common/baseUrl.js
@@ -1,14 +1,2 @@
-import { Platform } from "react-native";
-
-let baseURL = `http://${process.env.EXPO_PUBLIC_HOSTNAME}:3000/api/v1/`; // <-- Check if server is running on port 3000, otherwise change here the PORT
-
-// let myExpoIP = "192.168.1.139"; // <---- Change here to your IP address
-
-// let baseURL = `http://${myExpoIP}:3000/api/v1/`; // <-- Check if server is running on port 3000, otherwise change here the PORT
-
-// let baseURL =
-//   Platform.OS == "android"
-//     ? (baseURL = `${process.env.EXPO_PUBLIC_HOSTNAME}/api/v1/`) // <----- Type here you IP if you are using Android
-//     : (baseURL = `${process.env.EXPO_PUBLIC_HOSTNAME}/api/v1/`); // <----- Type here you IP if you are using iOS
-
+let baseURL = "https://api.learnium.app/api/";
 export default baseURL;
